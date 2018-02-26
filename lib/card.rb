67f546800be73024c3fbb6cc5b21ed594e5361dd
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-02-26 15:08:11 -0800

    Deck exists, Two tests have passed: Can exsist and will have 52 cards on initialization

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,30 @@
 
 # card.rb
-
 class Card
-  attr_reader :value, :suit
+  attr_reader :suit
+  attr_accessor :value
 
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
-
+    if value < 1 || value > 13
+      raise ArgumentError.new("This is an invalid card value #{value}. Acceptable range is 1-13.")
+    end
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    if value == 1
+      face_value = "Ace"
+    elsif value == 11
+      face_value = "Jack"
+    elsif value == 12
+      face_value = "Queen"
+    elsif value == 13
+      face_value = "King"
+    else
+      face_value = value
+    end
+    return "#{face_value} of #{suit.to_s}"
   end
 
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,36 @@
-
 # deck.rb
 
 require_relative 'card'
 
 class Deck
+  # Be able to be instantiated.
+  # Be created with 52 Card objects as attributes.
+  attr_reader :deck
 
   def initialize
-
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    suits = ['spades','hearts', 'diamonds', 'clubs']
+    @deck = []
+    values.each do |value|
+      suits.each do |suit|
+        @deck << Card.new(value, suit)
+      end
+    end
   end
 
+
+  # Have a method called draw which removes a Card from the Deck and returns the removed Card.
   def draw
     # returns a card
   end
 
+  # Have a shuffle method
   def shuffle
     # shuffles the deck
+  end
+
+  # Have a count method which returns the number of cards in the Deck.
+  def count
+
   end
 end
